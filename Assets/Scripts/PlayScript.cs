53cf903c46f2111c8735f7a8466d09a1f55acc76
Wing Chau <ccw.wingc@gmail.com>
2017-01-09 08:18:22 +0000

    impletement audio script

---
+++ Assets/Scripts/PlayScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayScript : MonoBehaviour {
+
+    private AudioSource m_source;
+    private bool m_played = false;
+
+    [SerializeField]
+    private AudioClip m_clip;
+
+	// Use this for initialization
+	void Start () {
+        m_source = GetComponent<AudioSource>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnTriggerEnter(Collider other) {
+        if (!m_played) {
+            m_source.PlayOneShot(m_clip);
+        }
+    }
+}
