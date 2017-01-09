0f0385a588218726e73506ca97cb2ed518298813
Wing Chau <ccw.wingc@gmail.com>
2017-01-09 08:04:55 +0000

    implement door sfx

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -6,10 +6,19 @@ public class Door : MonoBehaviour {
     private bool m_isOpened = false;
     private bool m_trigger = false;
     public float doorOpenAngle = 90.0f;
+
     [SerializeField]
     private float m_speed = 2.0f;
     [SerializeField]
+    private AudioClip m_openDoor;
+    [SerializeField]
+    private float m_openDoorVol = 1.0f;
+    [SerializeField]
+    private AudioClip m_closeDoor;
+    [SerializeField] float m_closeDoorVol = 1.0f;
+
     private Transform m_transform;
+    private AudioSource m_audioSource;
     private Vector3 m_defaultVector, m_targetVector;
 
     // Use this for initialization
@@ -17,12 +26,19 @@ public class Door : MonoBehaviour {
         m_transform = this.gameObject.transform;
         m_defaultVector = m_transform.eulerAngles;
         m_targetVector = new Vector3(m_defaultVector.x, m_defaultVector.y + doorOpenAngle, m_defaultVector.z);
+
+        m_audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update() {
         if (Input.GetKeyDown("e") && m_trigger) {
             m_isOpened = !m_isOpened;
+            if (m_isOpened) {
+                m_audioSource.PlayOneShot(m_openDoor, m_openDoorVol);
+            } else {
+                m_audioSource.PlayOneShot(m_closeDoor, m_closeDoorVol);
+            }
         }
 
         if (m_isOpened) {
